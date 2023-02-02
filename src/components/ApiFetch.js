6fa3d64bb2e711364ee034ecbc476334388539b4
A <tsc953u@gmail.com>
2023-02-03 01:14:50 +0900

    APIデータ取得方法

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -1,27 +1,39 @@
 import React , {useState,useEffect} from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 
 
 
 const ApiFetch = () => {
+  
   const [posts,setPosts] = useState([])
+  const [id,setId] = useState(1)
+  const [clicked,setClicked] = useState(false)
+
+  const handkerClicked = () => {
+    setClicked(!clicked)
+  }
 
   useEffect( () =>{
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(res => {
-      setPosts(res.data)
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{mehtod:'GET'})
+    .then(res => res.json() )
+    .then(data => {
+      setPosts(data)
     })
+  },[clicked])
 
-  })
 
 
   return (
     <div>
-      <ul>
+      <input type='text' value={id} onChange={evt=>setId(evt.target.value)}/>
+      <button type='button' onClick={handkerClicked}>Get post</button>
+      {posts.id}
+      {/* <ul>
         {
             posts.map(post => <li key={post.id}>{post.id} {post.title}</li>)
         }
-      </ul>
+      </ul> */}
+
     </div>
   )
 }
